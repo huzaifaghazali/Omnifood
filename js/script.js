63d5e1d514d625220674d473b66b075dfd3a8ec5
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-05-16 22:35:52 +0500

    Add sticky navigation

--- js/script.js
+++ js/script.js
@@ -42,3 +42,31 @@ allLinks.forEach(function (link) {
   });
 });
 
+///////////////////////////////////////////////////////////
+// Sticky navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      // document.querySelector(".header").classList.add('sticky');
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      // document.querySelector(".header").classList.remove('sticky');
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
